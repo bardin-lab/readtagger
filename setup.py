08756836e38efe754a9f57c8e6f6b11f0031b591
mvdbeek <m.vandenbeek@gmail.com>
2017-02-16 14:53:04 +0100

    Prepare setup.py for bumpversion

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ try:
 except ImportError:
     from distutils.core import setup
 
+__VERSION__ = '0.1.5'
+
 requirements = ['contextlib2', 'pysam', 'six', 'shutilwhich']
 
 ENTRY_POINTS = '''
@@ -13,7 +15,7 @@ ENTRY_POINTS = '''
 
 setup(
     name='tag_reads',
-    version='0.1.5',
+    version=__VERSION__,
     packages=['tag_reads'],
     install_requires=requirements,
     entry_points=ENTRY_POINTS,
