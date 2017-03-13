a08591246e596e398fda4d8b56b05a20e2ed2c8b
mvdbeek <m.vandenbeek@gmail.com>
2017-03-13 12:41:52 +0100

    Rework GFF output

--- readtagger/gff_io.py
+++ readtagger/gff_io.py
@@ -1,6 +1,4 @@
-import collections
 from functools import partial
-import yaml
 from concurrent.futures import ThreadPoolExecutor
 from BCBio import GFF
 from Bio.Seq import Seq
@@ -40,21 +38,14 @@ def get_feature(cluster, sample, i, blast=None):
                   "right_insert": cluster.right_contigs,
                   "ID": "%s_%d" % (sample, i),
                   "valid_TSD": cluster.valid_tsd}
+    feature = SeqFeature(FeatureLocation(cluster.start, cluster.end), type="TE", strand=1, qualifiers=qualifiers)
     if blast:
-        left_description, right_description, common_inserts = blast.blast_cluster(cluster)
-        qualifiers['left_description'] = yaml.dump(convert(left_description), default_flow_style=False).replace('\n', ' ').replace('-', '')
-        qualifiers['right_description'] = yaml.dump(convert(right_description), default_flow_style=False).replace('\n', ' ').replace('-', '')
-        qualifiers['common_inserts'] = yaml.dump(list(convert(common_inserts)), default_flow_style=False).replace('\n', ' ').replace('-', '')
-
-    return cluster.tid, SeqFeature(FeatureLocation(cluster.start, cluster.end), type="TE", strand=1, qualifiers=qualifiers)
-
-def convert(data):
-    """Convert unicode to bytestring"""
-    if isinstance(data, basestring):
-        return str(data)
-    elif isinstance(data, collections.Mapping):
-        return dict(map(convert, data.iteritems()))
-    elif isinstance(data, collections.Iterable):
-        return type(data)(map(convert, data))
-    else:
-        return data
+        seqfeature_args = blast.blast_cluster(cluster).to_feature_args()
+        base_args = {'location': FeatureLocation(cluster.start, cluster.end), 'strand': 1}
+        subfeatures = []
+        for feature_args in seqfeature_args:
+            args = base_args.copy()
+            args.update(feature_args)
+            subfeatures.append(SeqFeature(**args))
+        feature.sub_features = subfeatures
+    return cluster.tid, feature
