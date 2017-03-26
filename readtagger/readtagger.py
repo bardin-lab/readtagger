92cd5b316b5d4706511c89e93bbbf1c73b635c2a
mvdbeek <m.vandenbeek@gmail.com>
2017-03-26 20:39:30 +0200

    Bump version: 0.3.9 → 0.3.10

--- readtagger/readtagger.py
+++ readtagger/readtagger.py
@@ -16,7 +16,7 @@ from .tags import (
     BaseTag,
 )
 
-__VERSION__ = '0.3.9'
+__VERSION__ = '0.3.10'
 
 
 class SamTagProcessor(object):
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-__VERSION__ = '0.3.9'
+__VERSION__ = '0.3.10'
 
 ENTRY_POINTS = '''
         [console_scripts]
