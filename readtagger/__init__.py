9b9334bf4c0364e3e06e64a9b3e532d0b10f93ba
mvdbeek <m.vandenbeek@gmail.com>
2017-05-09 16:22:23 +0200

    Bump version: 0.3.21 → 0.3.22

--- readtagger/__init__.py
+++ readtagger/__init__.py
@@ -1,3 +1,3 @@
 """readtagger basepackage."""
 
-VERSION = '0.3.21'
+VERSION = '0.3.22'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-__VERSION__ = '0.3.21'
+__VERSION__ = '0.3.22'
 
 ENTRY_POINTS = '''
         [console_scripts]
