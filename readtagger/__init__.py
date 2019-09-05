e7efafa6c7103ef5d11d8b72c19a2aafd51a6aaa
mvdbeek <m.vandenbeek@gmail.com>
2019-09-05 15:51:32 +0200

    Bump version: 0.5.15 → 0.5.16

--- readtagger/__init__.py
+++ readtagger/__init__.py
@@ -1,3 +1,3 @@
 """readtagger basepackage."""
 
-VERSION = '0.5.15'
+VERSION = '0.5.16'
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__VERSION__ = '0.5.15'
+__VERSION__ = '0.5.16'
 
 ENTRY_POINTS = '''
         [console_scripts]
